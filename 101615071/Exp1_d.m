1767183bd707249e65f40a086131406fa396af88
gitvintess <36266297+gitvintess@users.noreply.github.com>
2018-08-02 19:46:41 +0100

    Update Exp1_d.m

--- 101615071/Exp1_d.m
+++ 101615071/Exp1_d.m
@@ -2,4 +2,17 @@
 clc
 clear all
 n=-10:1:10
-x=0.8^n
+x = (0.8).^n;
+x_ = (0.8).^(-n);
+
+even = (x + x_)/2;
+
+subplot(2,1,1)
+stem(n,even)
+title('Even')
+
+odd = (x - x_)/2;
+
+subplot(2,1,2)
+stem(n,odd)
+title('odd')
